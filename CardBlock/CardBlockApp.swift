825ce16d69d0a93fd25b7c146305f6f3097a1895
Tamerlan Swift <majn39143@gmail.com>
2025-10-17 13:05:21 +0500

    Initial Commit

---
+++ CardBlock/CardBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardBlockApp.swift
+//  CardBlock
+//
+//  Created by Tamerlan Swift on 17.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct CardBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
